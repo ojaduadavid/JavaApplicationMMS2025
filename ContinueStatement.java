f7a7317ab17c7e29a3c20ce7b887b4814e1bbc2c
ojaduadavid <davidojadua@gmail.com>
2026-03-05 01:28:41 -0800

    third file

--- ContinueStatement.java
+++ ContinueStatement.java
@@ -1,5 +1,6 @@
 public class ContinueStatement{
 	public static void main(String[] args){
+		
 		for(int i = 5; i <= 20; i++){
 			if(i == 15){
 				continue;
---
+++ UsingMathClass.java
@@ -0,0 +1,9 @@
+public class UsingMathClass{
+	public static void main(String[] args){
+		System.out.println("The absolute value is " + Math.abs(-60));
+		System.out.println("The greater number is " + Math.max(25,90));
+		System.out.println("The square root of the number is " + Math.sqrt(25));
+		System.out.println("The result of 6 raise to the power of 2 is " + Math.sqrt(25));
+		System.out.println("The random number generated is " + Math.random());
+	}
+}
